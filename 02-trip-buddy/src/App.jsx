e2072c6262814a898223e50a9ca98ab478e6d7c4
iamharshtambe <harshtambe.dev@gmail.com>
2025-02-11 21:55:45 +0530

    modified form component

--- 02-trip-buddy/src/App.jsx
+++ 02-trip-buddy/src/App.jsx
@@ -3,7 +3,7 @@ import './index.css';
 const initialItems = [
    { id: 1, description: 'Passports', quantity: 2, packed: false },
    { id: 2, description: 'Socks', quantity: 12, packed: false },
-   { id: 2, description: 'Bags', quantity: 12, packed: true },
+   { id: 3, description: 'Bags', quantity: 12, packed: true },
 ];
 
 export default function App() {
@@ -22,7 +22,24 @@ function Logo() {
 }
 
 function Form() {
-   return <div className="add-form">What do you need for your 😍 trip ?</div>;
+   function handleSubmit(e) {
+      e.preventDefault();
+   }
+
+   return (
+      <form className="add-form" onSubmit={handleSubmit}>
+         <h3>What do you need for your 😍 trip ?</h3>
+         <select>
+            {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+               <option value={num} key={num}>
+                  {num}
+               </option>
+            ))}
+         </select>
+         <input type="text" placeholder="Item..." />
+         <button>Add</button>
+      </form>
+   );
 }
 
 function PackingList() {
@@ -30,7 +47,7 @@ function PackingList() {
       <div className="list">
          <ul>
             {initialItems.map((item) => (
-               <Item key={item.id} itemObject={item} />
+               <Item itemObject={item} key={item.id} />
             ))}
          </ul>
       </div>
