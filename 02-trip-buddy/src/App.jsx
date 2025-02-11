814ed554c7fa51d91fcd9cb6175a69c55e78eab2
iamharshtambe <harshtambe.dev@gmail.com>
2025-02-11 19:23:32 +0530

    built layout

--- 02-trip-buddy/src/App.jsx
+++ 02-trip-buddy/src/App.jsx
@@ -1,9 +1,28 @@
-function App() {
+import './index.css';
+
+export default function App() {
    return (
-      <div className="App">
-         <h1>Hello</h1>
+      <div className="app">
+         <Logo />
+         <Form />
+         <PackingList />
+         <Stats />
       </div>
    );
 }
 
-export default App;
+function Logo() {
+   return <h1>🏖 Trip Buddy 🧳</h1>;
+}
+
+function Form() {
+   return <div className="add-form">What do you need for your 😍 trip ?</div>;
+}
+
+function PackingList() {
+   return <div className="list">List</div>;
+}
+
+function Stats() {
+   return <div className="stats">Stats</div>;
+}
