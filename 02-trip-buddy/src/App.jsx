02234e0c285e1f512c182cdf0170fa8ec1dc7c03
iamharshtambe <harshtambe.dev@gmail.com>
2025-02-12 21:09:56 +0530

    added controlled elements

--- 02-trip-buddy/src/App.jsx
+++ 02-trip-buddy/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './index.css';
 
 const initialItems = [
@@ -21,22 +22,42 @@ function Logo() {
    return <h1>🏖 Trip Buddy 🧳</h1>;
 }
 
+// CONTROLLED ELEMENTS -> 1. create piece of state 2. use state as value of input field 3. onchange
+
 function Form() {
+   const [description, setDescription] = useState('');
+   const [quantity, setQuantity] = useState(1);
+
    function handleSubmit(e) {
       e.preventDefault();
+
+      if (!description) return;
+
+      const newItem = { description, quantity, packed: false, id: Date.now() };
+
+      setDescription('');
+      setQuantity(1);
    }
 
    return (
       <form className="add-form" onSubmit={handleSubmit}>
          <h3>What do you need for your 😍 trip ?</h3>
-         <select>
+         <select
+            value={quantity}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+         >
             {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
                <option value={num} key={num}>
                   {num}
                </option>
             ))}
          </select>
-         <input type="text" placeholder="Item..." />
+         <input
+            type="text"
+            placeholder="Item..."
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+         />
          <button>Add</button>
       </form>
    );
